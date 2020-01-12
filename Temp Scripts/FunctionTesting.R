099cb749154bba2be9a89caf364a7877d880a404
SOLV-Code <gottfries.pestal@solv.ca>
2020-01-12 15:47:37 -0800

    offset testing

--- Temp Scripts/FunctionTesting.R
+++ Temp Scripts/FunctionTesting.R
@@ -7,4 +7,13 @@ plotCorrMatrix(M$cor.mat)
 plotCorrMatrix(M$cor.mat,order="hclust",n.groups=4)
 
 
+cor(mtcars)
+cor(mtcars[,1], mtcars[,2])
+
+cor(mtcars[1:31,1],mtcars[2:32,2])
+cor(mtcars[1:30,1],mtcars[3:32,2])
+cor(mtcars[2:31,1],mtcars[1:30,2])
+
+ccf(mtcars[,1], mtcars[,2], lag.max = 4 , type = "correlation",
+    plot = FALSE, na.action = na.omit)
 
