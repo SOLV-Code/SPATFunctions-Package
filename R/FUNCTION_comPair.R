5de5e77afd92acd78ab319efcb6cbd6c26d21721
SOLV-Code <gottfries.pestal@solv.ca>
2020-02-07 01:49:21 -0800

    comPair: added plotly plot

--- R/FUNCTION_comPair.R
+++ R/FUNCTION_comPair.R
@@ -20,6 +20,7 @@ na.idx <-  !complete.cases(X[,2:3])
 fit.cor.cumul <- runCor(X[!na.idx,2],X[!na.idx,3], n = window, sample = TRUE, cumulative = TRUE)
 fit.cor.window <- runCor(X[!na.idx,2],X[!na.idx,3], n = window, sample = TRUE, cumulative = FALSE)
 
+if(plot.type=="print"){
 
 plot(1:5,1:5,type="n",bty="n",xlab=names(X)[1],ylab = "Correlation",
      ylim=c(-1,1),xlim=range(X[,1]))
@@ -29,15 +30,36 @@ lines(X[!na.idx,1], fit.cor.cumul,type="o",pch=19,col="darkblue",cex=0.5)
 legend("topleft",legend=c("Window","Cumulative"),pch=c(NA,19),col=c("lightblue","darkblue")
        ,ncol=2,lty=1,bty="n")
 title(main = paste(names(X)[2],"vs.",names(X)[3]))
+}
+
+if(plot.type=="shiny"){
+
+x.corr <- as.data.frame(cbind(X[!na.idx,1], fit.cor.cumul,fit.cor.window,lb=-0.5,ub=0.5))
+
+names(x.corr) <- c("x_vec","Cumulative","Window","lb","ub") # plotly needs data frame names, so need to standardize
+#print(x.corr)
+
+p <- plot_ly(x.corr, x = ~x_vec, y = ~Cumulative, name = "Cumulative", type = "scatter", mode = "lines+markers") %>%
+  add_trace(y = ~Window, name = "Window", mode = "lines",
+            line = list(color = 'lightgrey', width = 2 )) %>%
+  add_trace(y = ~lb, name = "", mode = "lines",
+            line = list(color = 'red', width = 2 ,dash = 'dash'), showlegend = FALSE) %>%
+  add_trace(y = ~ub, name = "", mode = "lines",
+            line = list(color = 'red', width = 2 ,dash = 'dash'), showlegend = FALSE) %>%
+        layout(title = "", #paste(names(X)[2],"vs.",names(X)[3]),
+         xaxis = list(title = "Years"),
+         yaxis = list(title = "Correlation",range=c(-1,1)),
+         legend = list(orientation = 'h',x = min(x.corr[,1]), y = 1))
 
 
-
-
-
+}
 
 X.out <- list(End= X[!na.idx,1],cumul.corr = fit.cor.cumul,window.corr = fit.cor.window )
 names(X.out)[1] <- names(X)[1]
-return(X.out)
+
+if(plot.type == "print"){return(list(corr.out = X.out))}
+
+if(plot.type == "shiny"){return(list(corr.out = X.out,plot = p))}
 
 } # end plotCorrMatrix
 
